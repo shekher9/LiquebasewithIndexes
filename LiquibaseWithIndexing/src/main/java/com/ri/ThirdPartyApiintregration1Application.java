de5298c93ca46bf4257f9677b0cc9978c6c03014
shekher <sundershekher9@gmail.com>
2020-01-03 10:30:51 +0530

    First commit

---
+++ LiquibaseWithIndexing/src/main/java/com/ri/ThirdPartyApiintregration1Application.java
@@ -0,0 +1,13 @@
+package com.ri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdPartyApiintregration1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdPartyApiintregration1Application.class, args);
+	}
+
+}
